5f767fd820891f3fdcf94b481750df32fce6aa82
Djuro Drljaca <djurodrljaca@gmail.com>
2019-10-02 21:49:38 +0200

    Replaced "data directory" option with Qt resource files

--- code/tests/unit/ConfigReader/testConfigReader.cpp
+++ code/tests/unit/ConfigReader/testConfigReader.cpp
@@ -52,15 +52,12 @@ private slots:
     void testReadValidConfig();
 
 private:
-    // Holds the path to the data directory
-    QDir m_testDataDirPath;
 };
 
 // Test Case init/cleanup methods ------------------------------------------------------------------
 
 void TestConfigReader::initTestCase()
 {
-    m_testDataDirPath = QDir(TEST_DATA_DIR_PATH);
 }
 
 void TestConfigReader::cleanupTestCase()
@@ -82,7 +79,7 @@ void TestConfigReader::cleanup()
 void TestConfigReader::testReadValidConfig()
 {
     // Read config file
-    const QString configFilePath(m_testDataDirPath.absoluteFilePath("ValidConfig.json"));
+    const QString configFilePath(QStringLiteral(":/TestData/ValidConfig.json"));
     ConfigReader configReader;
 
     auto config = configReader.read(configFilePath);
@@ -90,7 +87,73 @@ void TestConfigReader::testReadValidConfig()
 
     // Check config configs
     QCOMPARE(config->type(), ConfigNode::Type::Object);
+    QVERIFY(config->isRoot());
     QCOMPARE(config->count(), 1);
+
+    // Check "/root_node"
+    QVERIFY(config->containsMember("root_node"));
+    const auto *rootNode = config->member("root_node");
+    QVERIFY(rootNode != nullptr);
+    QVERIFY(rootNode->isObject());
+    QCOMPARE(rootNode->count(), 1);
+
+    // Check "/root_node/sub_node"
+    {
+        QVERIFY(rootNode->containsMember("sub_node"));
+        const auto *subNode = rootNode->member("sub_node");
+        QVERIFY(subNode != nullptr);
+        QVERIFY(subNode->isObject());
+        QCOMPARE(subNode->count(), 2);
+
+        // Check "/root_node/sub_node/sub_node1"
+        {
+            QVERIFY(subNode->containsMember("sub_node1"));
+            const auto *subNode1 = subNode->member("sub_node1");
+            QVERIFY(subNode1 != nullptr);
+            QVERIFY(subNode1->isObject());
+            QCOMPARE(subNode1->count(), 2);
+
+            // Check "/root_node/sub_node/sub_node1/bool_param"
+            QVERIFY(subNode1->containsMember("bool_param"));
+            const auto *boolParam = subNode1->member("bool_param");
+            QVERIFY(boolParam != nullptr);
+            QVERIFY(boolParam->isValue());
+            QCOMPARE(boolParam->value(), QVariant(true));
+
+            // Check "/root_node/sub_node/sub_node1/array_param"
+            QVERIFY(subNode1->containsMember("array_param"));
+            const auto *arrayParam = subNode1->member("array_param");
+            QVERIFY(arrayParam != nullptr);
+            QVERIFY(arrayParam->isArray());
+            QCOMPARE(arrayParam->count(), 3);
+
+            const int values[3] = {5, 6, 7};
+
+            for (int i = 0; i < 3; i++)
+            {
+                const auto *element = arrayParam->element(i);
+                QVERIFY(element != nullptr);
+                QVERIFY(element->isValue());
+                QCOMPARE(element->value(), QVariant(values[i]));
+            }
+        }
+
+        // Check "/root_node/sub_node/sub_node2"
+        {
+            QVERIFY(subNode->containsMember("sub_node2"));
+            const auto *subNode2 = subNode->member("sub_node2");
+            QVERIFY(subNode2 != nullptr);
+            QVERIFY(subNode2->isObject());
+            QCOMPARE(subNode2->count(), 1);
+
+            // Check "/root_node/sub_node/sub_node2/int_param"
+            QVERIFY(subNode2->containsMember("int_param"));
+            const auto *intParam = subNode2->member("int_param");
+            QVERIFY(intParam != nullptr);
+            QVERIFY(intParam->isValue());
+            QCOMPARE(intParam->value(), QVariant(1));
+        }
+    }
 }
 
 // Main function -----------------------------------------------------------------------------------
